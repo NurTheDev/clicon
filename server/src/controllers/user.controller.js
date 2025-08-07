199e17312138d6f0b2e97e9e2fe3db5a1e98c389
NurTheDev <nur756.islam@gmail.com>
2025-08-07 17:58:36 +0600

    working on nodeMailer

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,7 +3,9 @@ const customError = require("../utils/customError")
 const asyncHandler = require("../helpers/asyncHandler")
 const {success} = require("../utils/apiResponse")
 const {userValidation} = require("../validators/user.validator")
-
+const {generateOTP} = require("../utils/otp")
+const {verifyEmail} = require("../templet/emailTemplet")
+const {sendEmail} = require("../helpers/sendEmail")
 /**
  * Register user
  * @type {(function(*, *, *): Promise<void>)|*}
@@ -16,5 +18,11 @@ exports.register = asyncHandler(async (req, res) => {
     if(!user){
         throw new customError("User registration failed", 400)
     }
+    // Get OTP
+    const {otp, expiry} = generateOTP()
+    const verifyLink = `http://localhost:3000/verify?email=${email}&otp=${otp}`
+    // Send OTP to user through email
+    const emailVerifyTemplate = verifyEmail(name, otp, expiry, verifyLink)
+    await sendEmail(email, emailVerifyTemplate)
     success(res, "User registered successfully", user, 201)
 })
---
+++ server/src/helpers/sendEmail.js
@@ -0,0 +1,19 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    secure: process.env.NODE_ENV === "production",
+    auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.APP_PASSWORD.replace(/_/g, ' ')
+    },
+});
+exports.sendEmail = async (email, template) => {
+    const info = await transporter.sendMail({
+        from: "Clicon",
+        to: email,
+        subject: "Email verification",
+        html: template
+    })
+    console.log("Message sent: %s", info.messageId);
+}
