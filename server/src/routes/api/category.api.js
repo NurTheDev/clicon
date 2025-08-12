2cdadf8c9ebc7d8c85d278f8a3bb2266a3f34f35
NurTheDev <nur756.islam@gmail.com>
2025-08-12 17:40:27 +0600

    working

--- server/src/routes/api/category.api.js
+++ server/src/routes/api/category.api.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../../controllers/category.controller");
-const authGard = require("../../middleware/authGard.middleware")
+const authGard = require("../../middleware/authGard.middleware");
+const upload = require("../../middleware/multer.middleware");
 
 router.route("/create-category").post(authGard, categoryController.createCategory)
 
