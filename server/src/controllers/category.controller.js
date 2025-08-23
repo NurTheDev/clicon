55dc585d3bc4648334b3fde7b08c1d23c3b55b82
NurTheDev <nur756.islam@gmail.com>
2025-08-23 16:41:25 +0600

    working on cloudinary

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -3,8 +3,10 @@ const {customError} = require("../utils/customError")
 const {success} = require("../utils/apiResponse")
 const asyncHandler = require("../helpers/asyncHandler")
 const {categoryValidation} = require("../validators/category.validator");
+const {uploadImage} = require("../helpers/claudinary");
 
 exports.createCategory = asyncHandler(async (req, res) => {
     const {name, image} = await categoryValidation(req)
-    console.log(name, image)
+    const imgUrl = await uploadImage(image.path)
+    console.log(imgUrl)
 })
