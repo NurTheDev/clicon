feb108a529677ed2ea2ab6b05b5359cea07d33f7
NurTheDev <nur756.islam@gmail.com>
2025-09-08 15:14:46 +0600

    working

--- client/src/Pages/mainLayout/Home/component/HeroBanner.tsx
+++ client/src/Pages/mainLayout/Home/component/HeroBanner.tsx
@@ -3,36 +3,54 @@ import Container from "../../../../common/Container.tsx";
 import {getImgUrl} from "../../../../helpers";
 import PrimaryButton from "../../../../common/PrimaryButton.tsx";
 import Icons from "../../../../helpers/IconProvider.tsx";
+import {heroBannerInfo} from "../../../../data";
+import Slider from "../../../../helpers/Slider.tsx";
 
 const HeroBanner = () => {
+    console.log(heroBannerInfo)
     return (
         <div>
             <Container>
-                <div className={"grid grid-cols-1 gap-4 md:grid-cols-2 items-center lg:grid-cols-3"}>
-                    <div className={"lg:col-span-2 rounded-md bg-gray-50 lg:p-12 xl:p-14 p-6 md:p-10 flexRowBetween"}>
-                        <div className={"flexColumnStart gap-4 lg:w-1/2"}>
-                            <p className={"text-secondary-600 body-medium-600 font-public-sans"}>
-                                THE BEST PLACE TO PLAY
-                            </p>
-                            <h1 className={"text-gray-900 font-public-sans display3"}>Xbox Consoles</h1>
-                            <p className={"text-gray-700 font-public-sans label3"}>
-                                Save up to 50% on select Xbox games. Get 3 months of PC Game Pass for $2 USD.
-                            </p>
-                            <PrimaryButton
-                                className={
-                                    "body-medium-600 py-2 sm:py-3 text-gray-00 bg-primary-500 px-5 sm:px-4" +
-                                    " rounded-xs lg:px-8" +
-                                    " flex items-center gap-2 hover:scale-105 transition-all duration-200"
-                                }
-                                aria-label="Shop now"
-                            >
-                                <span>Shop Now</span>
-                                <span>{Icons.rightArrow}</span>
-                            </PrimaryButton>
-                        </div>
-                        <div>
-                            <img src={getImgUrl("ps5.png")} alt={"product image"}/>
-                        </div>
+                <div className={"grid grid-cols-1 gap-4 md:grid-cols-2 items-center lg:grid-cols-3 "}>
+                    <div className={"lg:col-span-2 rounded-md bg-gray-50 lg:p-12 xl:p-14 p-6 md:p-10 lg:min-h-[50vh]" +
+                        "flexColumnCenter " +
+                        " "}>
+                        <Slider>
+                            {heroBannerInfo.map((item, index) => (
+                                <div key={index}
+                                     className={"flexRowBetween flex flex-col-reverse lg:flex-row justify-between" +
+                                         " lg:gap-0 gap-8 relative"}>
+                                    <div className={"flexColumnStart gap-4"}>
+                                        <p className={"text-secondary-600 body-medium-600 font-public-sans"}>
+                                            {item.title}
+                                        </p>
+                                        <h1 className={"text-gray-900 font-public-sans display3"}>{item.heading}</h1>
+                                        <p className={"text-gray-700 font-public-sans label3"}>
+                                            {item.description}
+                                        </p>
+                                        <PrimaryButton
+                                            className={
+                                                "body-medium-600 py-2 sm:py-3 text-gray-00 bg-primary-500 px-5 sm:px-4" +
+                                                " rounded-xs lg:px-8" +
+                                                " flex items-center gap-2 hover:scale-105 transition-all duration-200"
+                                            }
+                                            aria-label="Shop now"
+                                        >
+                                            <span>Shop Now</span>
+                                            <span>{Icons.rightArrow}</span>
+                                        </PrimaryButton>
+                                    </div>
+                                    <div className={"max-h-[200px] lg:max-h-[300px] "}>
+                                        <img src={getImgUrl(item.image)} alt={"product image"}
+                                             className={"object-contain max-h-[200px] lg:max-h-[300px]"}/>
+                                        <p className={"lg:w-24 lg:h-24 w-14 h-14 rounded-full bg-secondary-500" +
+                                            " text-gray-00 font-public-sans text-[22px] font-semibold flexRowCenter" +
+                                            "absolute top-0 right-0 z-10"}>{item.price}</p>
+                                    </div>
+
+                                </div>
+                            ))}
+                        </Slider>
                     </div>
                     <div className={"grid grid-row-2 gap-4"}>
                         <div className={"bg-blue-400 h-10"}></div>
---
+++ client/src/helpers/Slider.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Pagination} from 'swiper/modules';
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+// @ts-ignore
+import 'swiper/css';
+// @ts-ignore
+import 'swiper/css/pagination';
+
+type SliderProps = {
+    children: React.ReactNode;
+    spaceBetween?: number;
+    slidesPerView?: number | 'auto';
+};
+const Slider: React.FC<SliderProps> = ({
+                                           children,
+                                           spaceBetween = 50,
+                                           slidesPerView = 1,
+                                       }) => {
+    const slides = React.Children.toArray(children).map((child, index) => (
+        <SwiperSlide key={index}>{child}</SwiperSlide>
+    ));
+    return (
+        <div >
+            <Swiper
+                pagination={{
+                    dynamicBullets: true,
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                spaceBetween={spaceBetween}
+                slidesPerView={slidesPerView}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
+                {slides}
+            </Swiper>
+        </div>
+    );
+};
+
+export default React.memo(Slider);
