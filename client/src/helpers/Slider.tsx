7d33945b3d52806b2c65757fb680070c547be94a
NurTheDev <nur756.islam@gmail.com>
2025-09-09 17:50:15 +0600

    working on swipper

--- client/src/helpers/Slider.tsx
+++ client/src/helpers/Slider.tsx
@@ -11,24 +11,37 @@ type SliderProps = {
     children: React.ReactNode;
     spaceBetween?: number;
     slidesPerView?: number | 'auto';
+    animationDuration?: number;
+    animation?: 'slide' | 'fade' | 'cube' | 'coverflow' | 'flip';
+    dots?: boolean;
+    autoPlay?: boolean;
+    loop?: boolean;
 };
 const Slider: React.FC<SliderProps> = ({
                                            children,
                                            spaceBetween = 50,
                                            slidesPerView = 1,
+                                           animationDuration = 3000,
+                                           animation = 'slide',
+                                           dots = true,
+                                           autoPlay = false,
+                                           loop = true,
                                        }) => {
     const slides = React.Children.toArray(children).map((child, index) => (
-        <SwiperSlide  key={index} >{child}</SwiperSlide>
+        <SwiperSlide key={index}>{child}</SwiperSlide>
     ));
     return (
-        <div >
+        <div>
             <Swiper
-                pagination={{
-                    dynamicBullets: true,
-                    clickable: true,
-                }}
+                pagination={dots ? {clickable: true, dynamicBullets: true} : false}
+                autoplay={autoPlay ? {delay: animationDuration, disableOnInteraction: false} : false}
+                effect={animation}
+                loop={true}
+                speed={animationDuration}
                 modules={[Pagination]}
+                className="mySwiper"
                 spaceBetween={spaceBetween}
+                grabCursor={true}
                 slidesPerView={slidesPerView}
                 onSwiper={(swiper) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
