ffdd5f4569f0ef7e1103c8385a778df21c8bcdb4
nurislam <nur756.islam@gmail.com>
2025-09-21 23:16:24 +0600

    added feature card

--- client/src/Pages/mainLayout/Home/Home.tsx
+++ client/src/Pages/mainLayout/Home/Home.tsx
@@ -1,10 +1,12 @@
 import HeroBanner from "./component/HeroBanner.tsx";
+import Features from "./component/Features.tsx";
 
 const Home = () => {
     return (
         <div>
             <div className={"mt-5 md:mt-10"}>
                 <HeroBanner/>
+                <Features/>
             </div>
         </div>
     );
--- client/src/Pages/mainLayout/Home/component/Features.tsx
+++ client/src/Pages/mainLayout/Home/component/Features.tsx
@@ -5,7 +5,9 @@ import Icons from "../../../../helpers/IconProvider.tsx";
 const Features = () => {
     const content: {
         icon: React.ReactNode,
-    } = [
+        title: string,
+        desc: string,
+    }[] = [
         {
             icon: Icons.box,
             title: "Fasted Delivery",
@@ -30,11 +32,16 @@ const Features = () => {
     return (
         <Container>
             <div className={"bg-gray-00 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-4 border" +
-                " border-gray-100 rounded-md-"}>
-                <div>
-                    <span>{Icons.box}</span>
-                    <div><h5>Fasted Delivery</h5></div>
-                </div>
+                " border-gray-100 rounded-md shadow-sm lg:mt-10 mt-5"}>
+                {content?.map((item, index) => (
+                    <div key={index} className={"flexRowCenter gap-x-5 p-4 border-r border-r-gray-100 " +
+                        "last:border-0 hover:bg-gray-50 transition-all duration-200 cursor-pointer"}>
+                        <span className={"text-4xl"}>{item.icon}</span>
+                        <div className={"flexColumnStart text-gray-900 font-public-sans gap-y-1"}><h5 className={"font-medium"}>{item.title}</h5>
+                            <p className={"text-sm"}>{item.desc}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </Container>
     );
