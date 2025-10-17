c81ff427f7a39d6a33c80e07d17b1d27bcefa9ba
Nur Islam <nur756.islam@gmail.com>
2025-10-17 11:41:47 +0600

    working

--- server/src/controllers/payment.controller.js
+++ server/src/controllers/payment.controller.js
@@ -16,5 +16,54 @@ exports.paymentSuccess = asyncHandler(async (req, res) => {
     if (!updateOrder) {
         throw new customError("Order not found for the given transaction ID", 404);
     }
-    res.redirect("https://temp-mail.org/en");
+    res.redirect("localhost:5173/payment/success");
 });
+
+exports.paymentFailure = asyncHandler(async (req, res) => {
+    console.log("Payment failure payload:", req.body);
+    const {tran_id, status } = req.body;
+    const updateOrder = await orderSchema.findOneAndUpdate(
+        { transactionId: tran_id },
+        {
+            paymentStatus: "FAILED",
+            paymentDetails: req.body
+        },
+        { new: true }
+    );
+    if (!updateOrder) {
+        throw new customError("Order not found for the given transaction ID", 404);
+    }
+    res.redirect("localhost:5173/payment/fail");
+})
+exports.paymentCancel = asyncHandler(async (req, res) => {
+    console.log("Payment cancel payload:", req.body);
+    const {tran_id, status } = req.body;
+    const updateOrder = await orderSchema.findOneAndUpdate(
+        { transactionId: tran_id },
+        {
+            paymentStatus: "FAILED",
+            paymentDetails: req.body
+        },
+        { new: true }
+    );
+    if (!updateOrder) {
+        throw new customError("Order not found for the given transaction ID", 404);
+    }
+    res.redirect("localhost:5173/payment/cancel");
+})
+exports.ipnHandler = asyncHandler(async (req, res) => {
+    console.log("IPN payload:", req.body);
+    const {tran_id, status } = req.body;
+    const updateOrder = await orderSchema.findOneAndUpdate(
+        { transactionId: tran_id },
+        {
+            paymentStatus: status === "VALID" ? "COMPLETED" : "FAILED",
+            paymentDetails: req.body
+        },
+        { new: true }
+    );
+    if (!updateOrder) {
+        throw new customError("Order not found for the given transaction ID", 404);
+    }
+    res.status(200).send('IPN received');
+})
