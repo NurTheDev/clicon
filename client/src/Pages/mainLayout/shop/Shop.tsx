c3f403ef63695af0fa7d88fa2616f04538b0b958
Nur Islam <nur756.islam@gmail.com>
2025-12-06 16:38:53 +0600

    wip: working on client side

--- client/src/Pages/mainLayout/shop/Shop.tsx
+++ client/src/Pages/mainLayout/shop/Shop.tsx
@@ -6,62 +6,7 @@ import ProductCard from "../../../common/ProductCard.tsx";
 import ProductCardSkeleton from "../../../skeletons/ProductCardSkeleton.tsx";
 import Icons from "../../../helpers/IconProvider.tsx";
 import SearchBar from "../../../common/SearchBar.tsx";
-interface SideBarLinksProps {
-    selectedCategory: string;
-    onSelect: (category: string) => void;
-}
-const SideBarLinks:React.FC<SideBarLinksProps> = ({selectedCategory, onSelect})=>{
-    const {data, isLoading} = useQuery({
-        queryKey: ['products'],
-        queryFn: async () => {
-            const response = await fetch('https://dummyjson.com/products/category-list');
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        },
-        staleTime: 5 * 60 * 1000, // 5 minutes
-    })
-    const [visibleCategories, setvisibleCategories] = React.useState<number>(5);
-    return (
-        <div>
-            <h2 className={"label2 mb-4"}>Category</h2>
-            <ul className={"flex flex-col gap-3"}>
-                {data?.slice(0, visibleCategories).map((category: string, index: number) => (
-                    isLoading ? (
-                        <li className="flex items-center gap-2 animate-pulse">
-                            <div className="w-5 h-5 bg-gray-300 rounded-full"></div>
-                            <div className="h-4 bg-gray-300 rounded w-24"></div>
-                        </li>
-                    ) : (
-                        <li
-                            key={index}
-                            className="flex items-center gap-2 py-2 last:border-b last:border-gray-100 last:pb-6"
-                        >
-                            <input
-                                type="radio"
-                                name="radio-5"
-                                className="radio text-primary-500 w-5 h-5"
-                                value={category}
-                                checked={selectedCategory === category}
-                                onClick={() => onSelect(category)}
-                            />
-                            <span className="ml-2 body-medium-400">{category.charAt(0).toUpperCase() + category.slice(1)}</span>
-                        </li>
-                    )
-                ))}
-                {data && data.length > visibleCategories && (
-                    <button
-                        className="mt-4 text-primary-500 body-medium-500 text-start flex items-center gap-1"
-                        onClick={() => setvisibleCategories(prev => prev + 5)}
-                    >
-                        <span>Show More </span> <span>{Icons.rightArrow}</span>
-                    </button>
-                )}
-            </ul>
-        </div>
-    )
-}
+import SideBarLinks from "./component/SideBar.tsx";
 const Shop:React.FC = () => {
     const [selectedCategory, setSelectedCategory] = React.useState<string>("beauty");
     const {data, isLoading} = useQuery({
